e99666757161775ed7c45fb529f0225c65965816
Громилов Максим, фронтенд подрядчик, команда Жукова Д <gromilov.m@etton.net>
2025-08-13 12:13:50 +0300

    start

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#102A42',
+        secondary: '#2FC6F5',
+        accent: '#2FF5E3',
+        dark: '#11100C',
+        light: '#173550',
+      },
+      fontFamily: {
+        gilroy: ['Gilroy', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
